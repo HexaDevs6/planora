d04a390212e0e7d1953fd2885ef3fcb663d5dad4
mahmoud-abu-attiya <mahmoud.abuattiya106@gmail.com>
2025-10-02 18:31:44 +0300

    init planora

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <main className="text-red-700">
+      <h1>Planora</h1>
+      <Button>Click me</Button>
+    </main>
+  )
+}
+
+export default App
