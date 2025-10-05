3eedd33ef181c8bcb9c5f9e7f73516cf052248f5
mahmoud-abu-attiya <mahmoud.abuattiya106@gmail.com>
2025-10-05 12:45:31 +0300

    Add internationalization support with i18next and language switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { Button } from "@/components/ui/button"
+import LanguageSwitcher from "./components/LanguageSwitcher"
+import { useTranslation } from 'react-i18next';
+
+
 
 function App() {
+  // useTranslation hook provides access to the translation function 't'
+  const { t } = useTranslation();
 
   return (
     <main className="text-red-700">
-      <h1>Planora</h1>
-      <Button>Click me</Button>
+      <LanguageSwitcher />
+      <h1>{t('hero.subtitle')}</h1>
     </main>
   )
 }
