baa6740932fbf32c970ef01d339b651b6c99f7b1
MEDO151 <alaaali2847@gmail.com>
2025-10-08 03:39:34 +0300

    Edit Join Section

--- src/components/JoinSection.jsx
+++ src/components/JoinSection.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function JoinSection() {
   return (
-    <footer className="static md:relative flex justify-center items-center">
+    <section className="static md:relative flex justify-center items-center">
       <div className="static md:absolute min-w-full md:min-w-70 -top-0 md:-top-50  drop-shadow-[0px_4px_4px_rgba(0,0,0,0.25)] bg-secondary py-12 px-2 md:px-30 rounded-0 md:rounded-sm text-center flex justify-center items-center flex-col gap-6">
         <h1 className="font-bold text-violet text-2xl md:text-3xl">
           Ready to Transform Your Events?
@@ -26,6 +26,6 @@ export default function JoinSection() {
         </form>
       </div>
       
-    </footer>
+    </section>
   );
 }
