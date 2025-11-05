a86e26b4e2c1442867c549e173b2a050712c27fd
mustafaHawash <hiohio855@gmail.com>
2025-11-06 01:44:33 +0200

    Refactor store configuration: replace searchSlice with dedicated slices for events and services

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
-import searchReducer from './searchSlice'
-import categoriesReducer from '@/store/categoriesSlice'
+import searchAndFilterEventsSlice from "./searchAndFilterEventsSlice";
+import searchAndFilterServiceSlice from "./searchAndFilterServiceSlice";
+import categoriesReducer from "@/store/categoriesSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    search: searchReducer,
     categories: categoriesReducer,
+    servicesSearchAndFilter: searchAndFilterServiceSlice,
+    eventsSearchAndFilter: searchAndFilterEventsSlice,
   },
 });
-
-export default store;
