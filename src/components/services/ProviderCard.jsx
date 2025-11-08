1a3fc5dd8d77c0c5cae648cbd7ce6fa95bf8c149
mustafaHawash <hiohio855@gmail.com>
2025-11-08 10:52:15 +0200

    ProviderCard and ServiceDetails components conditional formating change

--- src/components/services/ProviderCard.jsx
+++ src/components/services/ProviderCard.jsx
@@ -7,47 +7,49 @@ import { useState, useEffect } from "react";
 import Spinner from "@/components/SpinnerLoader";
 
 const ProviderCard = ({ provider_id }) => {
-   const [provider, setProvider] = useState(null);
-   const navigate = useNavigate();
-   const { lang } = useDirection();
-   const handleMessageClick = (e) => {
-      e.preventDefault();
-      navigate(`/messages/new?to=${provider_id}`);
-   };
-   const fetchProvider = async () => {
-      const { data, error } = await supabase
-         .from("users")
-         .select("*")
-         .eq("id", provider_id);
-         if (error) {
-            console.error(error);
-            return;
-         } else {
-            setProvider(data[0]);
-         }
-   };
-   useEffect(() => {
-      fetchProvider();
-   }, [provider_id]);
-   if (!provider) {
-      return <Spinner />;
-   }
-   return (
-      <Link to={`/user/${provider_id}`}>
-         <div className="group gradient-card rounded-xl p-4 flex  md:flex-col gap-4 items-center text-center">
-            <img
-               src={provider.image || "https://placehold.co/200"}
-               alt={provider.full_name}
-               className="md:w-24 h-full md:h-auto aspect-square object-cover rounded-full"
-            />
-            <div className="text-start md:text-center flex flex-col gap-2 items-start md:items-center w-full">
-               <h3 className="text-lg font-bold group-hover:underline">
-                  {provider.full_name}
-               </h3>
-               <p className="text-sm text-foreground">
-                  {provider.bio.length > 50 ? provider.bio?.slice(0, 50) + "..." : provider.bio || "Has no bio yet!"}
-               </p>
-               {/* <div className="flex items-center gap-2">
+  const [provider, setProvider] = useState(null);
+  const navigate = useNavigate();
+  const { lang } = useDirection();
+  const handleMessageClick = (e) => {
+    e.preventDefault();
+    navigate(`/messages/new?to=${provider_id}`);
+  };
+  const fetchProvider = async () => {
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", provider_id);
+    if (error) {
+      console.error(error);
+      return;
+    } else {
+      setProvider(data[0]);
+    }
+  };
+  useEffect(() => {
+    fetchProvider();
+  }, [provider_id]);
+  if (!provider) {
+    return <Spinner />;
+  }
+  return (
+    <Link to={`/user/${provider_id}`}>
+      <div className="group gradient-card rounded-xl p-4 flex  md:flex-col gap-4 items-center text-center">
+        <img
+          src={provider.image || "https://placehold.co/200"}
+          alt={provider.full_name}
+          className="md:w-24 h-full md:h-auto aspect-square object-cover rounded-full"
+        />
+        <div className="text-start md:text-center flex flex-col gap-2 items-start md:items-center w-full">
+          <h3 className="text-lg font-bold group-hover:underline">
+            {provider.full_name}
+          </h3>
+          <p className="text-sm text-foreground">
+            {provider.bio && provider.bio.length > 50
+              ? provider.bio?.slice(0, 50) + "..."
+              : provider.bio || "Has no bio yet!"}
+          </p>
+          {/* <div className="flex items-center gap-2">
                   <Star
                      className="w-4 h-4 text-amber"
                      fill={provider.rating >= 1 ? "currentColor" : "none"}
@@ -56,18 +58,18 @@ const ProviderCard = ({ provider_id }) => {
                      {provider.rating}
                   </span>
                </div> */}
-               <button
-                  onClick={handleMessageClick}
-                  className="mt-1 flex items-center justify-center gap-2 px-4 py-2 rounded-lg bg-violet text-white font-semibold hover:bg-violet/80 transition-colors"
-                  type="button"
-               >
-                  <MessageCircle className="w-4 h-4" />
-                  {lang === "en" ? "Message" : "مراسله"}
-               </button>
-            </div>
-         </div>
-      </Link>
-   );
+          <button
+            onClick={handleMessageClick}
+            className="mt-1 flex items-center justify-center gap-2 px-4 py-2 rounded-lg bg-violet text-white font-semibold hover:bg-violet/80 transition-colors"
+            type="button"
+          >
+            <MessageCircle className="w-4 h-4" />
+            {lang === "en" ? "Message" : "مراسله"}
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
 };
 
 export default ProviderCard;
--- src/pages/ServiceDetails.jsx
+++ src/pages/ServiceDetails.jsx
@@ -10,69 +10,67 @@ import { useState, useEffect } from "react";
 import Spinner from "@/components/SpinnerLoader";
 
 const ServiceDetails = () => {
-   const { lang } = useDirection();
-   const { serviceId } = useParams();
-   const [service, setService] = useState(null);
+  const { lang } = useDirection();
+  const { serviceId } = useParams();
+  const [service, setService] = useState(null);
 
-   // const [service, setService] = useState(null);
-   const [loading, setLoading] = useState(true);
-   useEffect(() => {
-      const fetchService = async () => {
-         setLoading(true);
-         const { data, error } = await supabase
-            .from("services")
-            .select("*")
-            .eq("id", serviceId);
-         if (error) {
-            console.error(error);
-            return;
-         } else {
-            setLoading(false);
-            setService(data[0]);
-            console.log(data[0]);
-         }
-      };
-      fetchService();
-   }, [serviceId]);
-   if (loading) {
-      return <Spinner />;
-   }
-   const getImages = (images) => {
-      if (images.type === "string") {
-         return JSON.parse(images);
+  // const [service, setService] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchService = async () => {
+      setLoading(true);
+      const { data, error } = await supabase
+        .from("services")
+        .select("*")
+        .eq("id", serviceId);
+      if (error) {
+        console.error(error);
+        return;
+      } else {
+        setLoading(false);
+        setService(data[0]);
+        console.log(data[0]);
       }
-      return images;
-   };
-   return (
-      <main className="container">
-         <DetailsHero
-            lang={lang}
-            img={service.thumbnail}
-            title={lang === "ar" ? service.name_ar : service.name}
-         />
-         <div className="grid grid-cols-1 md:grid-cols-6 gap-4 md:gap-8">
-            <div className="md:col-span-2">
-               <ProviderCard provider_id={service.client_id} />
-            </div>
-            <div className="md:col-span-4">
-               <section className="py-8 md:py-12">
-                  <h3 className="text-2xl font-bold mb-4 text-gradient-amber">
-                     {lang === "en" ? "About the Service" : "حول الخدمة"}
-                  </h3>
-                  <p className="text-foreground leading-8 text-lg">
-                     {lang === "ar"
-                        ? service.description_ar
-                        : service.description}
-                  </p>
-               </section>
-               {service.images.length > 0 && (
-                  <ServiceGallery images={getImages(service.images)} />
-               )}
-               {/* <ServiceReviews reviews={service.reviews_list} /> */}
-            </div>
-         </div>
-      </main>
-   );
+    };
+    fetchService();
+  }, [serviceId]);
+  if (loading) {
+    return <Spinner />;
+  }
+  const getImages = (images) => {
+    if (images.type === "string") {
+      return JSON.parse(images);
+    }
+    return images;
+  };
+  return (
+    <main className="container">
+      <DetailsHero
+        lang={lang}
+        img={service.thumbnail}
+        title={lang === "ar" ? service.name_ar : service.name}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-6 gap-4 md:gap-8">
+        <div className="md:col-span-2">
+          <ProviderCard provider_id={service.client_id} />
+        </div>
+        <div className="md:col-span-4">
+          <section className="py-8 md:py-12">
+            <h3 className="text-2xl font-bold mb-4 text-gradient-amber">
+              {lang === "en" ? "About the Service" : "حول الخدمة"}
+            </h3>
+            <p className="text-foreground leading-8 text-lg">
+              {lang === "ar" ? service.description_ar : service.description}
+            </p>
+          </section>
+          {service.images && (
+            <ServiceGallery images={getImages(service.images)} />
+          )}
+          {/* <ServiceReviews reviews={service.reviews_list} /> */}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ServiceDetails;
