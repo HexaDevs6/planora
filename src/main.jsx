3fa98702f700ef230580e1479f8fcb2b56178d82
mustafaHawash <hiohio855@gmail.com>
2025-11-23 20:22:03 +0200

    - Moves auth listener setup to App component
    - Initializes authentication listener within the main app
    lifecycle to ensure proper cleanup and avoid redundant setups.
    Improves code structure by removing listener invocation from entry point.

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,10 @@ import "./index.css";
 import App from "./App";
 import "./i18n"; // Initialize i18n before rendering App
 import { store } from "@/store/store";
-import { startAuthListener } from "@/store/authListener";
-startAuthListener(store);
+
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
         <Provider store={store}>
             <App />
         </Provider>
-    </StrictMode>
-);
+)
